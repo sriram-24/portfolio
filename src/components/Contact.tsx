bfe9adfb6f652957dcacf44f1aff2fbf8216bc27
sriram-24 <sriram1999m@gmail.com>
2025-01-01 20:01:12 +0530

    feat(cotnact): contact section added.

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { motion } from "motion/react"
+import { ContactType, user, type IContact } from '@/constants/user'
+import { EnvelopeOpen, GithubLogo, LinkedinLogo } from '@phosphor-icons/react'
 
 function Contact() {
 	return (
@@ -13,11 +15,18 @@ function Contact() {
 			}}
 		>
 			<h4 className='mb-4'>Contact</h4>
-			<div className='flex flex-wrap gap-4 justify-normal'>
+			<div className='flex flex-wrap gap-12 justify-center items-center m-6 bg-contact text-contact-foreground h-64 p-6 rounded-lg'>
 				{
-
+					user.contacts.map((contact: IContact, index) =>
+						<div className='min-h-16 min-w-16  p-2 flex flex-col justify-center items-center rounded-lg' >
+							{
+								contact.type == ContactType.email ? <a href={contact.link} target='_blank' ><EnvelopeOpen size={32} alt={contact.type} /></a> :
+									contact.type == ContactType.github ? <a href={contact.link} target='_blank' ><GithubLogo size={32} alt={contact.type} /></a> :
+										contact.type == ContactType.linkedin ?  <a href={contact.link} target='_blank' ><LinkedinLogo size={32} alt={contact.type} /></a> : <></>
+							}
+						</div>
+					)
 				}
-				hello
 			</div>
 		</motion.div>
 	)
