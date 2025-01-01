95431eafe5616365beca6fd78be0d334ad7491fb
sriram-24 <sriram1999m@gmail.com>
2025-01-01 12:33:14 +0530

    fix: output set to static.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+	output:'static',
   integrations: [react(), tailwind({
 	applyBaseStyles : false
   })]
